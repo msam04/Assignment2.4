c5dc30e8fe1c9298955ff1628c7b23f600a62a53
msam04 <msam04@users.noreply.github.com>
2018-04-21 06:51:55 -0400

    Initial commit

---
+++ Python_Module_2_4.py
@@ -0,0 +1,12 @@
+
+# coding: utf-8
+
+# In[6]:
+
+
+str = "WE, THE PEOPLE OF INDIA, having solemnly resolved to constitute India into a SOVEREIGN, SOCIALIST, SECULAR, DEMOCRATIC REPUBLIC and to secure to all its citizens"
+
+str2 = "WE, THE PEOPLE OF INDIA,\n\thaving solemnly resolved to constitute India into a SOVEREIGN,\n\tSOCIALIST, SECULAR, DEMOCRATIC REPUBLIC\n\t and to secure to all its citizens".expandtabs()
+
+print(str2)
+
